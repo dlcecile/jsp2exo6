77f3456b5b752b20d24f026f65e3602b1c5fa1e3
DL <deleucecile@outlook.fr>
2019-10-28 11:13:20 +0100

    exo6 fin

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function changeimg(img,id)
+{
+  var id=id;
+  var src=img;
+document.getElementById(id).src=img;
+}
+function rechangeimg (img,id)
+{
+  var id=id;
+  var src=img;
+document.getElementById(id).src=img;
+}
